d35bf9200f74353ee83e4a1bf9f4cb23bfda0e9a
Christos Chatzifountas <heefoo@outlook.com>
2023-10-07 16:02:57 +0300

    Add basic web server

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,35 @@
-use actix_web::{HttpServer,  middleware::Logger, get, post, App, HttpResponse, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder, Result};
+use serde::{Serialize};
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+#[derive(Serialize)]
+pub struct Response {
+    pub message: String
+
+}
+#[get("/health")]
+async fn healthcheck( ) -> impl Responder{
+    let response = Response {
+     message: "Health check OK".to_string()
+    };
+    HttpResponse::Ok().json(response)
+
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body);
-}
+async fn not_found(  ) -> Result<HttpResponse>
+{
+    let response = Response {
+        message: "Resourse not found".to_string()
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!");
+    };
+    Ok(HttpResponse::NotFound().json(response))
 }
-
 
 #[actix_web::main]
-
 async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "debug") ;
-    std::env::set_var("RUST_BACKTRACE", "1");
-
-    env_logger::init();
-
-    HttpServer::new(|| {
-        let logger = Logger::default();
-
-        App::new()
-            .wrap(logger)
-            .service(hello)
-            .service (echo)
-            .route("/hey",web::get(.to(manual_hello)))
-    })
-    .bind(("127.0.0.1", 9080))?
+    HttpServer::new(|| App::new()
+                    .service(healthcheck)
+                    .default_service(web::route().to(not_found)))
+        .bind(("127.0.0.1", 9090))?
         .run()
         .await
-
-
-        // println!("Hello, world!");
 }
